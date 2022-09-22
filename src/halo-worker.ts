ea0a9011552077a16b01734500c36ccff05e78f8
lsankar4033 <lsankar4033@gmail.com>
2022-09-22 10:39:47 -0700

    Hook up test web client again

--- src/halo-worker.ts
+++ src/halo-worker.ts
@@ -1,59 +1,70 @@
-import { expose } from 'comlink';
-
-async function get_play_diff() {
-    console.log('diffing');
-    const multiThread = await import('halowordle');
-    await multiThread.default();
-    await multiThread.initThreadPool(navigator.hardwareConcurrency);
-    multiThread.init_panic_hook();
-    const ret = multiThread.get_play_diff("fluff", ["fluff", "fluff", "fluff", "fluff", "fluff", "fluff"]);
-    return ret;
-}
+import { expose } from "comlink";
 
 async function fetch_params() {
-    const response = await fetch('http://localhost:3000/params.bin');
-    const bytes = await response.arrayBuffer();
-    const params = new Uint8Array(bytes);
-    return params;
+  const response = await fetch("http://localhost:3000/params.bin");
+  const bytes = await response.arrayBuffer();
+  const params = new Uint8Array(bytes);
+  return params;
 }
 
-async function prove_play() {
-    const params = await fetch_params();
-    console.log("param length", params.length);
-    console.log("params", params);
+async function prove_fib() {
+  const params = await fetch_params();
+  console.log("param length", params.length);
+  console.log("params", params);
 
-    console.log('genning proof');
-    const multiThread = await import(
-        'halowordle'
-      );
-    await multiThread.default();
-    await multiThread.initThreadPool(navigator.hardwareConcurrency);
-    console.log('here we go');
-    const ret = multiThread.prove_play("fluff", ["fluff", "fluff", "fluff", "fluff", "fluff", "fluff"], params);
-    return ret;
+  console.log("genning proof");
+  const multiThread = await import("halowasm");
+  await multiThread.default();
+  await multiThread.initThreadPool(navigator.hardwareConcurrency);
+  console.log("here we go");
+  const ret = multiThread.prove_fib(BigInt(1), BigInt(1), params);
+  return ret;
 }
 
-async function verify_play(proof: any, diffs_js: any) {
-    const params = await fetch_params();
-    console.log("param length", params.length);
-    console.log("params", params);
+// async function get_play_diff() {
+//     console.log('diffing');
+//     const multiThread = await import('halowordle');
+//     await multiThread.default();
+//     await multiThread.initThreadPool(navigator.hardwareConcurrency);
+//     multiThread.init_panic_hook();
+//     const ret = multiThread.get_play_diff("fluff", ["fluff", "fluff", "fluff", "fluff", "fluff", "fluff"]);
+//     return ret;
+// }
 
-    console.log('verifying proof');
-    const multiThread = await import(
-        'halowordle'
-      );
-    await multiThread.default();
-    await multiThread.initThreadPool(navigator.hardwareConcurrency);
-    console.log('here we go');
-    const ret = multiThread.verify_play("fluff", proof, diffs_js, params);
-    return ret;
-}
+// async function prove_play() {
+//     const params = await fetch_params();
+//     console.log("param length", params.length);
+//     console.log("params", params);
 
+//     console.log('genning proof');
+//     const multiThread = await import(
+//         'halowordle'
+//       );
+//     await multiThread.default();
+//     await multiThread.initThreadPool(navigator.hardwareConcurrency);
+//     console.log('here we go');
+//     const ret = multiThread.prove_play("fluff", ["fluff", "fluff", "fluff", "fluff", "fluff", "fluff"], params);
+//     return ret;
+// }
+
+// async function verify_play(proof: any, diffs_js: any) {
+//     const params = await fetch_params();
+//     console.log("param length", params.length);
+//     console.log("params", params);
+
+//     console.log('verifying proof');
+//     const multiThread = await import(
+//         'halowordle'
+//       );
+//     await multiThread.default();
+//     await multiThread.initThreadPool(navigator.hardwareConcurrency);
+//     console.log('here we go');
+//     const ret = multiThread.verify_play("fluff", proof, diffs_js, params);
+//     return ret;
+// }
 
 const exports = {
-    get_play_diff,
-    prove_play,
-    verify_play
+  prove_fib,
 };
 export type HaloWorker = typeof exports;
 
